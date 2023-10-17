edcbc6da40a10cd8099bdef3fef3e889df3048a0
Fermen1305 <fermen1305@gmail.com>
2023-10-16 20:04:34 -0500

    search youtube querys

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Scraper = require('@yimura/scraper').default;
+
+const app = express();
+const port = 3000;
+
+app.get('/getListOfVideos', (req, res) => {
+  try {
+    const { song, artist } = req.query;
+    const youtube = new Scraper();
+
+    youtube.search(`${ song } ${ artist }`).then(results => {
+      res.send(results.videos);
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server Listen ${port}`);
+});
+
