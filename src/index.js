8c27002d3fc00a51f61788730a640e3403ff29b9
Fermen1305 <fermen1305@gmail.com>
2023-10-19 14:50:07 -0500

    test in production

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const Scraper = require('@yimura/scraper').default;
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send("hello word");
+app.get('/getListOfVideos', async (req, res)  => {
+  try {
+    const { song, artist } = req.query;
+    const youtube = new Scraper();
+
+    await youtube.search(`${ song } ${ artist }`).then(results => {
+      res.send(results.videos);
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 
+app.get('/download-video', async (req, res) => {
+
+});
 
 app.listen(port, () => {
-  console.log(`Server Listen ${port}`);
+  console.log(`Server On Listen ${port}`);
 });
-
